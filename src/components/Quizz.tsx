2d21313522727953d4357fb03cdef4af406fc543
Valentin GEREST <valentin@galadrim.fr>
2021-09-14 22:33:50 +0200

    update

--- src/components/Quizz.tsx
+++ src/components/Quizz.tsx
@@ -4,7 +4,7 @@ const pickNumber = (numbers: number[]): number => {
   return numbers[Math.floor(Math.random() * numbers.length)];
 };
 
-const numbers1 = [/*2, 3, 4, 5, */ 6, 7, 8 /* 9 */];
+const numbers1 = [2, 3, 4, 5, 6, 7, 8, 9];
 const numbers2 = numbers1;
 
 const QUIZZ_SIZE = 30;
